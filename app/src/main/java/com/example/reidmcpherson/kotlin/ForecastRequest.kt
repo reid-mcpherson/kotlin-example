962798898c3b4f744c8b66b4a2322ddc5b8b48f1
Reid McPherson <reid.mcpherson@gmail.com>
2021-08-26 11:36:41 -0700

    Added constant

--- app/src/main/java/com/example/reidmcpherson/kotlin/ForecastRequest.kt
+++ app/src/main/java/com/example/reidmcpherson/kotlin/ForecastRequest.kt
@@ -10,6 +10,7 @@ class ForecastRequest(val zipCode: String) {
         private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
         private val URL_PART = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
         private val COMPLETE_URL = "${URL_PART}&APPID=$APP_ID&zip="
+        private val TEST = "TEST"
     }
 
     fun execute(): ForecastResult {
